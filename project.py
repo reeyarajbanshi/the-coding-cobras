790ce41f39a5c766ee9501dcfad5e986b8edbb29
reeya <115366438+reeyarajbanshi@users.noreply.github.com>
2023-10-08 15:02:42 -0700

    project_updated

--- project.py
+++ project.py
@@ -9,16 +9,21 @@ right_motor = port.C
 forklift = port.F
 dozer = port.E
 
+motor_pair.pair(motor_pair.PAIR_1,port.A, port.C)
+
 async def forklift_up():
-    motor.run_for_degrees(port.F,250,280)
+    motor.run_for_degrees(forklift,250,280)
 
 async def forklift_down():
-    motor.run_for_degrees(port.F,-200,280)
+    motor.run_for_degrees(forklift,-200,280)
 
+async def dozer_up():
+    motor.run_for_degrees(dozer,300,300)
+
+async def dozer_down():
+    motor.run_for_degrees(dozer,-300,300)
 
 async def mission_1():
-    # Pair motors on port A and C
-    motor_pair.pair(motor_pair.PAIR_1,port.A, port.C)
     # moving forklift down
     # await motor.run_for_degrees(forklift,-100, 280)
     # Move straight at a specific velocity for 1 second; moves forward
@@ -51,11 +56,9 @@ async def mission_2():
         # move forklift up
         await motor.run_for_degrees(forklift,280,300)
     # await motor.run_for_degrees(forklift,-250,280)
-        
+
 
 async def mission_3():
-    # pairing motor A and C to motor pair
-    motor_pair.pair(motor_pair.PAIR_1,port.A,port.C)
     # moving forward toward tallest mission
     await motor_pair.move_for_time(motor_pair.PAIR_1,3000,0,velocity=280)
     # turning left towards mission 3
@@ -66,7 +69,18 @@ async def mission_3():
     await motor.run_for_degrees(forklift,-300,300)
 
 async def mission_4():
-    # pairing motor A and C to motor pair
-    motor_pair.pair(motor_pair.PAIR_1,port.A,port.C)
+    # moving dozer down to put mission between dozer and robot
+    # await motor.run_for_degrees(dozer,-300,300)
+    # moving robot forward to reach tallest mission
+    await motor_pair.move_for_time(motor_pair.PAIR_1,2800,0,velocity=-275)
+    # turning left to turn towards mission
+    await motor.run_for_degrees(right_motor,90,300)
+    # moving forward to go to mission
+    await motor_pair.move_for_time(motor_pair.PAIR_1,1000,0,velocity=-250)
+    # moving dozer up to complete mission
+    # # await motor.run_for_degrees(dozer,300,300)
+    # # moving backwards to go back to original destination
+    # await motor_pair.move_for_time(motor_pair.PAIR_1,2800,1,velocity = 275)
 
-runloop.run(mission_3())
+
+runloop.run(mission_4())
